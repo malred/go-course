e263de6b1dbf4d0ca84ca483b314da25accabdd1
malred <13695024677@163.com>
2024-09-17 18:19:44 +0800

    if for

--- 2-process_control/main.go
+++ 2-process_control/main.go
@@ -3,11 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	// 条件判断
 	a := 100
 	b := 100
-	// 条件判断
-
-	// if - else if - else
+	// if elseif else
 	if a > b {
 		fmt.Println("a>b")
 	} else if a == b {
@@ -16,30 +15,36 @@ func main() {
 		fmt.Println("a<b")
 	}
 
-	// switch 判断多个
+	// switch 多个条件
 	switch a {
 	case 100:
-		fmt.Println("100")
+		fmt.Println("a=100")
 	case 200:
-		fmt.Println("200")
-	case 300:
-		fmt.Println("300")
+		fmt.Println("a=200")
 	default:
-		// 如果上面的都不匹配, 就执行default
+		// 如果其他的都不匹配, 就走default
 		fmt.Println("???")
 	}
 
 	// 循环
-	// 定义i为0, 循环条件是i<5, 每次循环i+1
+
+	// 定义i, 如果i<5就继续循环, 每次循环后i+1
+	i := 0
+
+	for i < 5 {
+		fmt.Println(i)
+		i++
+	}
+
+	// 另一种写法
 	for i := 0; i < 5; i++ {
-		// 循环五次
 		fmt.Println(i)
 	}
 
-	// 运行看看和上一个的不同之处
+	// break & continue
 	for i := 0; i < 5; i++ {
 		if i == 1 {
-			fmt.Println("跳出当前这次循环, 但是不打断")
+			fmt.Println("跳出当前这一次循环, 但是不打断")
 			continue
 		}
 		if i == 3 {
@@ -47,15 +52,5 @@ func main() {
 			break
 		}
 		fmt.Println(i)
-	}
-
-	// 另一种
-	i := 1
-
-	for i < 5 {
-
-		fmt.Println(i)
-
-		i++
 	}
 }
